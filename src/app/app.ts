01ff84ffe8a5fbd64b478536da426596e748b668
Alexander Zimdahl <azimdahl91@gmail.com>
2026-01-08 13:54:14 -0500

    fix: remove unused RouterOutlet import from app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import { Header } from "./header/header";
 import { WorkCenters } from "./components/work-centers/work-centers";
 import { Timeline } from "./components/timeline/timeline";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, WorkCenters, Timeline],
+  imports: [Header, WorkCenters, Timeline],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
